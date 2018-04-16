58e3901ef0e7970d559bf9fee15e8a945cd5be29
stevlulz <sidahmedlahmer@gmail.com>
2018-04-16 16:05:45 +0100

    Modify Poly

--- lfsr/LFSR.cpp
+++ lfsr/LFSR.cpp
@@ -50,13 +50,17 @@ LFSR::LFSR(){
 
 /**
  *
- * in  = Xor of all REGi  where  i < 20 or (i<10 && 3*i% 2 == 0 )
+ * in  = Xor of all REGi  where  i < 20 or (i>40 && 3*i% 2 == 0 )
  *
  **/
 bool LFSR::generateNextBit(){
     bool tmp = this->reg.back();
     this->reg.pop_back();
-    bool in = reg[0]^tmp;
+    bool in = tmp;
+    for (int i = 0; i <SIZE ; ++i) {
+        if(i<20 or (i>40 && 3*i %2 == 0) )
+            in ^= this->reg.at(i);
+    }
     reg.push_front(in);
     return in;
 }
